57d20dd2e8c809aaefc933822eccebeabedf5398
Bladesheng <keadr23@gmail.com>
2022-09-24 18:33:09 +0200

    Add method for adding players to players list

--- src/components/PlayerPicker.tsx
+++ src/components/PlayerPicker.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import EditableButton from "./EditableButton";
 
+type IPlayers = {
+  [playerName: string]: boolean; // false == not selected, true == selected
+};
+
 export default function PlayerPicker() {
+  const [players, setPlayers] = useState<IPlayers>({});
+
   function addPlayer(playerName: string) {
+    if (playerName in players) return; // abort if name is duplicate
+
+    const playersCopy = structuredClone(players);
+    playersCopy[playerName] = false; // player is turned off by default
+    setPlayers(playersCopy);
+
     console.log("Added player: ", playerName);
+  }
+
+  function removePlayer(playerName: string) {
+    console.log("Removed player: ", playerName);
   }
 
   return (
